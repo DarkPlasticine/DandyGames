9c0c7078ce5b987f5d4b7e0244913871277163be
DarkPlasticine <marshalvasil@yandex.ru>
2017-01-05 13:48:32 +0000

    Добавлены две темы и переключение между ними

--- DandyGames_WPF/DandyGames_WPF/MainWindow.xaml.cs
+++ DandyGames_WPF/DandyGames_WPF/MainWindow.xaml.cs
@@ -23,6 +23,31 @@ namespace DandyGames_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            List<string> styles = new List<string> { "DarkBlue", "DarkOrange" };
+            cmbxTheme.SelectionChanged += ThemeChange;
+            cmbxTheme.ItemsSource = styles;
+            cmbxTheme.SelectedItem = "DarkBlue";
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = cmbxTheme.SelectedItem as string;
+
+            var uri = new Uri(@"Theme\"+ style + @"\Brushes.xaml", UriKind.Relative);
+            var core = new Uri(@"Theme\" + style + @"\Core.xaml", UriKind.Relative);
+
+            Application.Current.Resources.Clear();
+
+            ResourceDictionary resCore = Application.LoadComponent(core) as ResourceDictionary;
+            ResourceDictionary resDict = Application.LoadComponent(uri) as ResourceDictionary;
+
+            Application.Current.Resources.MergedDictionaries.Add(resCore);
+            Application.Current.Resources.MergedDictionaries.Add(resDict);
+
+            //resDict = Application.LoadComponent(core) as ResourceDictionary;
+
+            //Application.Current.Resources.MergedDictionaries.Add(resDict);
         }
     }
 }
